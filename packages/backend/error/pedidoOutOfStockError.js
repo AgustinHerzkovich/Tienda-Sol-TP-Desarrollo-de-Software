7fd34403fd7f8ec65a0cbd7ae253b8dd01ae3c45
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-10-08 18:13:01 -0300

    chore: agrego cantidad en stock al mensaje de error

--- packages/backend/error/pedidoOutOfStockError.js
+++ packages/backend/error/pedidoOutOfStockError.js
@@ -3,8 +3,8 @@ import AppError from './appError.js';
 export default class PedidoOutOfStockError extends AppError {
   constructor(itemsSinStock) {
     const itemsAsString = itemsSinStock
-      .map((item) => item.producto.id + ' - ' + item.producto.titulo + ' - ' + item.cantidad)
-      .join(' | ');
+      .map((item) => 'id: ' + item.producto.id + ' - nombre: ' + item.producto.titulo + ' - solicitado: ' + item.cantidad + ' - disponible: ' + item.producto.stock)
+      .join(' || ');
 
     super(
       `No hay stock suficiente para crear el Pedido. Items sin stock: ${itemsAsString}`,
