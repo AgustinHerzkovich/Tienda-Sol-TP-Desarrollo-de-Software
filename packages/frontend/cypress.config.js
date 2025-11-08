64a4663385b211882afd56b9166be5ac68bffc0c
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2025-11-08 16:11:45 -0300

    test: test E2E de compra de producto

--- packages/frontend/cypress.config.js
+++ packages/frontend/cypress.config.js
@@ -1,9 +1,14 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
+require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: process.env.FRONTEND_URL || 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      config.env.FRONTEND_URL =
+        process.env.FRONTEND_URL || 'http://localhost:3000';
+      return config;
     },
   },
 });
