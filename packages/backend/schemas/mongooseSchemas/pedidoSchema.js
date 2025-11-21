040bd86dc9a49663dff06513010f4e889feddde5
AgustinHerzkovich <aherzkovich@frba.utn.edu.ar>
2025-11-21 14:46:10 -0300

    feat: agrego timestamps en pedidos

--- packages/backend/schemas/mongooseSchemas/pedidoSchema.js
+++ packages/backend/schemas/mongooseSchemas/pedidoSchema.js
@@ -14,125 +14,131 @@ import { EstadoPedido } from '../../models/estadoPedido.js';
     this.historialEstados = [{fecha Date, estado String enum, ref a pedido, ref a usuario, motivo String}];
 */
 
-const PedidoSchema = new mongoose.Schema({
-  comprador: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Usuario',
-    required: true,
-  },
-  items: [
-    {
-      producto: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Producto',
-        required: true,
+const PedidoSchema = new mongoose.Schema(
+  {
+    comprador: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Usuario',
+      required: true,
+    },
+    items: [
+      {
+        producto: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Producto',
+          required: true,
+        },
+        cantidad: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+        precioUnitario: {
+          type: Number,
+          required: true,
+          min: 0,
+        },
+        _id: false,
       },
-      cantidad: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-      precioUnitario: {
-        type: Number,
-        required: true,
-        min: 0,
-      },
-      _id: false,
-    },
-  ],
-  total: {
-    type: Number,
-    required: true,
-  },
-  moneda: {
-    type: String,
-    enum: Object.values(Moneda),
-    required: true,
-  },
-  direccionEntrega: {
-    calle: {
-      type: String,
+    ],
+    total: {
+      type: Number,
       required: true,
     },
-    altura: {
+    moneda: {
       type: String,
+      enum: Object.values(Moneda),
       required: true,
     },
-    piso: {
-      type: String,
-      required: true,
-    },
-    departamento: {
-      type: String,
-      required: true,
-    },
-    codigoPostal: {
-      type: String,
-      required: true,
-    },
-    ciudad: {
-      type: String,
-      required: true,
-    },
-    provincia: {
-      type: String,
-      required: true,
-    },
-    pais: {
-      type: String,
-      required: true,
-    },
-    lat: {
-      type: String,
-      required: true,
-    },
-    lon: {
-      type: String,
-      required: true,
-    },
-  },
-  estado: {
-    type: String,
-    enum: Object.values(EstadoPedido).map((e) => e.valor),
-  },
-  fechaCreacion: {
-    type: Number,
-    required: true,
-  },
-  historialEstados: [
-    {
-      fecha: {
-        type: Date,
-        required: true,
-      },
-      estadoPrevio: {
-        type: String,
-        enum: Object.values(EstadoPedido).map((e) => e.valor),
-        required: true,
-      },
-      nuevoEstado: {
-        type: String,
-        enum: Object.values(EstadoPedido).map((e) => e.valor),
-        required: true,
-      },
-      pedido: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Pedido',
-        required: true,
-      },
-      usuario: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Usuario',
-        required: false, // Opcional porque puede ser un cambio automático del sistema
-      },
-      motivo: {
+    direccionEntrega: {
+      calle: {
         type: String,
         required: true,
       },
-      _id: false,
+      altura: {
+        type: String,
+        required: true,
+      },
+      piso: {
+        type: String,
+        required: true,
+      },
+      departamento: {
+        type: String,
+        required: true,
+      },
+      codigoPostal: {
+        type: String,
+        required: true,
+      },
+      ciudad: {
+        type: String,
+        required: true,
+      },
+      provincia: {
+        type: String,
+        required: true,
+      },
+      pais: {
+        type: String,
+        required: true,
+      },
+      lat: {
+        type: String,
+        required: true,
+      },
+      lon: {
+        type: String,
+        required: true,
+      },
     },
-  ],
-});
+    estado: {
+      type: String,
+      enum: Object.values(EstadoPedido).map((e) => e.valor),
+    },
+    fechaCreacion: {
+      type: Number,
+      required: true,
+    },
+    historialEstados: [
+      {
+        fecha: {
+          type: Date,
+          required: true,
+        },
+        estadoPrevio: {
+          type: String,
+          enum: Object.values(EstadoPedido).map((e) => e.valor),
+          required: true,
+        },
+        nuevoEstado: {
+          type: String,
+          enum: Object.values(EstadoPedido).map((e) => e.valor),
+          required: true,
+        },
+        pedido: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Pedido',
+          required: true,
+        },
+        usuario: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Usuario',
+          required: false, // Opcional porque puede ser un cambio automático del sistema
+        },
+        motivo: {
+          type: String,
+          required: true,
+        },
+        _id: false,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    collection: 'pedidos',
+  }
+);
 
 PedidoSchema.pre(/^find/, function (next) {
   this.populate({
